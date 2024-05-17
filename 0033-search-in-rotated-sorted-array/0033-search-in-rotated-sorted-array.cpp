f3e4ebc62b0ed164fe06be547ed013ed91219d04
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-05-17 09:55:25 +0530

    Time: 0 ms (100%), Space: 13.5 MB (35.34%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       int low = 0, high = nums.size() - 1;
+       
+        int start=0;
+        int end=nums.size()-1;
+        //int pos=-1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
+    /*
+    IN ROTATED ARRAY SEARCH THE MOST KEY THING IS TO FIRST FIND THE SORTED PART AND IF ELEMENTS LIE IN THAT
+     PART THEN DO SEARCH IN THAT PART AND ELEMENATE THE OTHER PART
+    */
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
                 return mid;
-            }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+            }else if(nums[mid]>=nums[start] ){
+                if(nums[mid]>=target && target>=nums[start]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+                
+            }else if(nums[mid]<=nums[end]){
+                if(nums[mid]<=target && target<= nums[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
                 }
+                
             }
         }
-
         return -1;
         
     }
