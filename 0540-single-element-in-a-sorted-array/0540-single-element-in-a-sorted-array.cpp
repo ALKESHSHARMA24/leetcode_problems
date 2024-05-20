420ea74d4c5afe0e88464b5486f905cbad7a71f6
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-05-20 12:32:24 +0530

    Time: 19 ms (54.24%), Space: 24.5 MB (99.09%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (mid % 2 == 1) {
-                mid--;
-            }
-            if (nums[mid] != nums[mid + 1]) {
-                right = mid;
-            } else {
-                left = mid + 2;
-            }
+        int start=1;
+        int end=nums.size()-1;
+        int singleElement=0;
+
+        // TO ELIMINATE THE OVERHEAD OF EDGE CASES WE WILL JUST FIRSTLY CHECK ALL THAT EDGE CASES
+        if(nums.size()==1){
+            return nums[0];
         }
-        return nums[left];
+        if (nums[0]!=nums[1]){
+            return nums[0];
+        }if(nums[end]!=nums[end-1]){
+            return nums[end];
+        }else{
+            end=nums.size()-1;
+            while(start<=end){
+
+                int mid=start+(end-start)/2;
+                if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                    singleElement=nums[mid];
+                    break;
+                }else if((mid%2==0)){
+                    if (nums[mid]==nums[mid+1]){
+                        start=mid+1;
+                    }else{
+                        end=mid-1;
+                    }
+                }else if (mid%2!=0){
+                    if(nums[mid]==nums[mid+1]){
+                        end=mid-1;
+                    }else{
+                        start=mid+1;
+                    }
+                }
+
+            }
+            return singleElement;
+        }
+        
     }
 };
