d12d293dcffba440314cca75db66270b5eeae706
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-05-20 12:44:12 +0530

    Time: 22 ms (31.46%), Space: 24.6 MB (50.85%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -9,31 +9,37 @@ public:
         if(nums.size()==1){
             return nums[0];
         }
-        if (nums[0]!=nums[1]){
+        else if (nums[0]!=nums[1]){
             return nums[0];
-        }if(nums[end]!=nums[end-1]){
+        }
+        else if(nums[end]!=nums[end-1]){
             return nums[end];
-        }else{
+        }
+        else{
             end=nums.size()-1;
             while(start<=end){
 
                 int mid=start+(end-start)/2;
+
                 if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                     singleElement=nums[mid];
                     break;
-                }else if((mid%2==0)){
+
+                }
+                else if((mid%2==0)){
                     if (nums[mid]==nums[mid+1]){
                         start=mid+1;
                     }else{
                         end=mid-1;
                     }
-                }else if (mid%2!=0){
+                }
+                else 
                     if(nums[mid]==nums[mid+1]){
                         end=mid-1;
                     }else{
                         start=mid+1;
                     }
-                }
+                
 
             }
             return singleElement;
