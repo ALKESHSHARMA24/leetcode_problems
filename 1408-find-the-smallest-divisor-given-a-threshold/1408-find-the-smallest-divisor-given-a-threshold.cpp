969d8c73f672a3cc82589657b8f3ab2e208020c9
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-05-23 09:55:41 +0530

    Time: 34 ms (50.7%), Space: 24.7 MB (53.7%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -3,9 +3,10 @@ public:
     int smallestDivisor(vector<int>& nums, int threshold) {
         int start=0;
         int end=nums.size()-1;
-        int minDivisor=0;
-        int maxDivisor=1;
-        int largestValue=0;
+        int minDivisor=0; //always true answer as are dividing with the largest 
+        int maxDivisor=1; //may be possible if only one element case
+
+        int largestValue=0; //finding that largest element from the array
 
         for (int num : nums) {
             largestValue = std::max(largestValue, num);
@@ -13,23 +14,25 @@ public:
 
         minDivisor = largestValue;
 
-        while(maxDivisor<=largestValue){
+        while(maxDivisor<=largestValue){    //performs binary search on the possible answers 
 
             int mid=maxDivisor+(largestValue-maxDivisor)/2;
             int divisorSum=0;
             start=0;
             while(start<=end){
-                divisorSum+=ceil(nums[start]/(double)mid);
+                divisorSum+=ceil(nums[start]/(double)mid); //imp to type cast into double to use ceil functionality
                 start++;
             }
 
             if(divisorSum<=threshold){
-                if(mid<=minDivisor){
+
+                if(mid<=minDivisor){ //compare with old divisor to check if we get new smallest divisor
                     minDivisor=mid;
                 }
-                largestValue=mid-1; 
+
+                largestValue=mid-1; //there can be more smallest divisor possible
             }else{
-                maxDivisor=mid+1;
+                maxDivisor=mid+1;  //else divisor lies in the right part.
             }
 
         }
