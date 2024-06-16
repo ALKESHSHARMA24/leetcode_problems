e313e82ce908419b59fbdcc128a3737045a6986c
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-06-16 23:49:44 +0530

    Time: 93 ms (64.99%), Space: 61.7 MB (80.81%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void helper(int n, int k, int i, vector<vector<int>> &result, vector<int> &temp){
+        if(temp.size()==k){
+            result.push_back(temp);
+            return;
+        }
+        for(int j=i;j<n;j++){
+            temp.push_back(j+1);
+            helper(n,k,j+1,result,temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        vector<vector<int>> result;
+        helper(n,k,0,result,temp); 
+    return result;       
+    }
+};
