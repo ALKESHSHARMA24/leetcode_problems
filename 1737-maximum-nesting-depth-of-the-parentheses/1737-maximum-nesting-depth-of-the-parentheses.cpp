602d5a179bca82492ba451450801daf10f3e925b
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-07-09 22:42:25 +0530

    Time: 3 ms (41.78%), Space: 7.5 MB (22.18%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        //Stack approach 
-        int MaxDepth=0;
-        //int tempDepth=0;
-        stack<char>st;
+        //Without Stack approach 
+        int maxDepth=0;
+        int openCounter=0;
         for(int i=0;i<s.length();i++){
             if(s[i]=='('){
-                st.push(s[i]);
+                openCounter++;
             }else if(s[i]==')'){
-                //every time when we get a close bracket then we will check that what was the previous maximum numbers of open bracket and if it is greater than our previous MaxDepth then simpy update it.
-
-                if(st.size()>MaxDepth){
-                    MaxDepth=st.size();
+                if(openCounter>maxDepth){
+                    maxDepth=openCounter;
                 }
-                st.pop();
+                openCounter--;
             }
         }
-        return MaxDepth;
+        
+        return maxDepth;
     }
 }; 
