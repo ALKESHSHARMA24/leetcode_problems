08f64e9c5125685467a4322c0252f49808547fbb
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-08-23 12:11:36 +0530

    Time: 10 ms (46.56%), Space: 9.8 MB (15.87%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        //bruteforce:- find the all sub arrays then check the how many different character is occuring in that subarrays length and if it's exist more than the k then break;
-        int maxFreq=0;
-        for(int i=0;i<s.size();i++){
-            vector<int> arr(26,0);
-            int Curr_maxfreq=0;
-            for(int j=i;j<s.size();j++){
-                arr[s[j]-'A']++;
-                 Curr_maxfreq=max(Curr_maxfreq,arr[s[j]-'A']);
-                 //The formula to find the odd character  in the continguos subarray with same charcter
-                if(((j-i+1)-Curr_maxfreq)>k){
-                    break;
+        //better Apporach:- use two pointer and sliding window
+         int left=0;
+         int right=0;
+         int maxLen=0;
+         int maxFreq=0;
+          unordered_map<char,int>mp;
+         while(right<s.size()){
+            mp[s[right]]++;
+            maxFreq=max(maxFreq,mp[s[right]]);//1 1 2 2
+            //using the formula "(right-left+1)-maxFreq" i can calculate that how many different character are occuring in the the present window and if it exceeeds more than the k than i have reinitialise the window untill it comes underthan k and only then i will go forward. to increase my window using right pointer. 
+            while((right-left+1)-maxFreq>k){//0 1 1 2
+                mp[s[left]]--;
+                for(const auto ele:mp){
+                     maxFreq=max(maxFreq,ele.second);
                 }
-                maxFreq=max(j-i+1,maxFreq);
+                left++;
             }
- 
-        }
-        return maxFreq;;
+  
+            //Now my winddow is satisfying all the given constraints so now i can calculate the maxlength for the current window and update it where it needs.
+                maxLen=max(maxLen,right-left+1);//1 2 3 4
+            // }
+            right++;
+         }
+         return maxLen;
     }
 };
