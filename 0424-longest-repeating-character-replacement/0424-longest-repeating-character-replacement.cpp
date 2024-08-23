1aba2339eaa733a965e8702c94634005e33d2b08
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-08-23 12:45:35 +0530

    Time: 12 ms (38.58%), Space: 9.5 MB (36.99%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,26 +4,27 @@ public:
         //better Apporach:- use two pointer and sliding window
          int left=0;
          int right=0;
-         int maxLen=0;
+         int maxlen=0;
          int maxFreq=0;
+         int length=0;
           unordered_map<char,int>mp;
+           
          while(right<s.size()){
             mp[s[right]]++;
-            maxFreq=max(maxFreq,mp[s[right]]);//1 1 2 2
-            //using the formula "(right-left+1)-maxFreq" i can calculate that how many different character are occuring in the the present window and if it exceeeds more than the k than i have reinitialise the window untill it comes underthan k and only then i will go forward. to increase my window using right pointer. 
-            while((right-left+1)-maxFreq>k){//0 1 1 2
+            maxFreq=max(maxFreq,mp[s[right]]);
+            
+
+            if((right-left+1)-maxFreq>k){
                 mp[s[left]]--;
-                for(const auto ele:mp){
-                     maxFreq=max(maxFreq,ele.second);
-                }
                 left++;
             }
-  
-            //Now my winddow is satisfying all the given constraints so now i can calculate the maxlength for the current window and update it where it needs.
-                maxLen=max(maxLen,right-left+1);//1 2 3 4
-            // }
+           
+            if((right-left+1)-maxFreq<=k){
+                maxlen=max(maxlen,right-left+1);
+            }
             right++;
          }
-         return maxLen;
+         
+         return maxlen;
     }
 };
