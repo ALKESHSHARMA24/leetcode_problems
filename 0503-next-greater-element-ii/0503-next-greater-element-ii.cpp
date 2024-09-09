b2176efd1ae8b53e69b0282385d73258ce06b0c0
ALKESH SHARMA <91245284+ALKESHSHARMA24@users.noreply.github.com>
2024-09-09 20:38:56 +0530

    Time: 19 ms (80.77%), Space: 27 MB (30.36%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int size = nums.size();
-        vector<int> result(size, -1);
+        //IF WE HAVE GIVEN THE CIRCULAR ARRAY IN THE PROBLEM DESCRIPTION
+        // THEN WE HAVE TO COMUPLSARY RUN THE LOOP AT LEAST FOR O(2N).
+        //FOR BOTH MONOTONIC AND THE NESTED LOOP 
 
-        for (int i = 0; i < size; i++) {
-            int currele = nums[i];
+        int size=nums.size();
+        vector<int>result(size,-1);
+        stack<int> st;
 
-            for (int j = 1; j < size; j++) {
-                int index = (j + i) % size;
+        for(int i=(size*2)-1;i>=0;i--){
+            int index= i%size;
+            int currEle=nums[index];
 
-                if (nums[index] > currele) {
-                    result[i] = nums[index];
-                    break;
-                }
+            while(!st.empty() && st.top()<=currEle){
+                st.pop();
             }
+
+            if(i<size && !st.empty()){
+                result[i]=st.top();
+            }
+            st.push(nums[index]);
         }
         return result;
+    
+
     }
 };
